aa6a80880565c509567f032418845f6f5cb0ef1a
Muhammad Nahid <mdnahid22@gmail.com>
2020-07-03 13:01:31 +0600

    settings

--- dj_shop/settings.py
+++ dj_shop/settings.py
@@ -24,7 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -106,4 +107,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
