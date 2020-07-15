35d6efa0379c266192758240b88f711bd023a142
Muhammad Nahid <mdnahid22@gmail.com>
2020-07-15 21:20:23 +0600

    Forgot Password Email

--- shop/urls.py
+++ shop/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('api/v1/token/refresh/', TokenRefreshView.as_view()),
     path('api/v1/current_user/', views.current_user),
     path('api/v1/register/', views.register),
+    path('api/v1/forgot_password/', views.forgot_password),
 
     path('api/v1/categories/', views.CategoryList.as_view()),
     path('api/v1/categories/<int:pk>/', views.CategoryDetail.as_view()),
--- shop/views/user_views.py
+++ shop/views/user_views.py
@@ -1,8 +1,9 @@
-
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.core.mail import EmailMultiAlternatives
 
 from shop.models import User
 from shop.serializers import UserSerializer
@@ -43,3 +44,52 @@ def register(request):
             status=status.HTTP_500_INTERNAL_SERVER_ERROR
         )
 
+
+# =====================================================================
+#                         Forgot Password
+# =====================================================================
+@api_view(['POST'])
+def forgot_password(request):
+    email = request.data['email'] if 'email' in request.data else None
+
+    # Validate
+    if not email:
+        return Response({'success': False, 'error': 'Email is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    # If user exists
+    user = User.objects.get(email=email)
+    if not user:
+        return Response({'success': False, 'error': 'User not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        # Generate Token
+        refresh = RefreshToken.for_user(user)
+        reset_token = str(refresh.access_token)
+        reset_url = f"http://localhost:8000/api/v1/reset_password/{reset_token}"
+
+        # Sending Mail
+        subject = "Reset Password - dj-shop"
+        mail_from = 'noreply@dj-shop.com'
+        mail_to = email
+        text_content = ''
+        html_content = (f'<div style="text-align: center; padding: 20px; line-height: 2; font-size: 1.2rem">'
+                        f'You are receiving this email because you (or someone else) have requested to reset a password. <br /> Make a PUT request to the following link to reset your password <br /><br />'
+                        f'<a href="{reset_url}" style="background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 1rem; font-weight: bold">'
+                        f'RESET PASSWORD'
+                        f'</a>'
+                        f'<p style="color: red;">This link will expire shortly</p>'
+                        f'</div>')
+
+        # send_mail(subject, text_content, mail_from, [mail_to], fail_silently=False)
+        msg = EmailMultiAlternatives(subject, text_content, mail_from, [mail_to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
+        return Response({'success': True, 'data': 'Password Reset Email Sent'})
+
+    except Exception as e:
+        print('\x1b[91m' + str(e) + '\x1b[0m')
+        return Response(
+            {'success': False, 'error': 'Something went wrong'},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
