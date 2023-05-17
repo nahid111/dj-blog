275d7c617aadd2198ae4ad270fcfb5c335a4d18f
Muhammad Nahid <mdnahid22@gmail.com>
2023-05-17 16:49:54 +0600

    Admin show custom fields

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import User, Category
+from django.utils.html import format_html
+from .models import User, Category, Post
 
-admin.site.register(User)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "category_list", "cover_image", "created_at", "updated_at",)
+
+    def category_list(self, obj):
+        return ", ".join([cat.title for cat in obj.categories.all()])
+
+    def cover_image(self, obj):
+        return format_html('<img width="100" src="{}" />'.format(obj.cover_img_url))
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
+admin.site.register(User)
